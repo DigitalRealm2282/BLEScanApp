da6f34ff12f23d277215fbdf63cacb33bcd30a6f
Digital Realm <113158980+DigitalRealm2282@users.noreply.github.com>
2023-05-19 00:58:17 +0300

    Delete apple/app/src/main directory

--- apple/app/src/main/data/DeviceDataBase.kt
+++
@@ -1,9 +0,0 @@
-//package com.ats.apple.data
-//
-//import androidx.room.Database
-//import androidx.room.RoomDatabase
-//
-//@Database(entities = [BaseDevice::class] , version = 1)
-//abstract class DeviceDataBase:RoomDatabase() {
-//    abstract val dao : DeviceDao
-//}
