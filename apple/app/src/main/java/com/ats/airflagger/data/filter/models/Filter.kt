a995970bbab7ac2051260e144632e3bc4f9bd068
Digital Realm <113158980+DigitalRealm2282@users.noreply.github.com>
2023-05-23 16:45:46 +0300

    Delete apple directory

--- apple/app/src/main/java/com/ats/airflagger/data/filter/models/Filter.kt
+++
@@ -1,8 +0,0 @@
-package com.ats.airflagger.data.filter.models
-
-import com.ats.airflagger.data.model.BaseDevice
-
-
-abstract class Filter {
-    abstract fun apply(baseDevices: List<BaseDevice>): List<BaseDevice>
-}
--- apple/app/src/main/java/com/ats/airflagger/util/risk/RiskLevel.kt
+++
@@ -1,5 +0,0 @@
-package com.ats.airflagger.util.risk
-
-enum class RiskLevel {
-    LOW, MEDIUM, HIGH
-}
